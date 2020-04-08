6cccbf77cdfe21a26116059ba900085b6d4db05f
David Schmotz <davidschmotz@gmx.de>
2020-04-08 12:35:59 +0200

    - fixed bugs

--- selectionsort.hs
+++ selectionsort.hs
@@ -1,25 +1,18 @@
 
-
+-- todo: integrated maximum and indexof function to make it more efficient
 
 selection_sort :: [Int] -> [Int]
 selection_sort arr
     | null arr = []
     | otherwise = (selection_sort (start ++ end)) ++ [maxi]
     where
-        maxi = getmax arr (arr !! 0)
+        maxi = maximum arr
         index = getindexof maxi arr
-        start = take (index-1) arr
+        start = take index arr
         end = drop (index+1) arr
 
 getindexof :: Int -> [Int] -> Int
 getindexof target (x:xs)
     | x == target = 0
     | otherwise = (getindexof target xs) + 1
-
-getmax :: [Int] -> Int -> Int
-getmax arr currMax 
-    | null arr              = currMax
-    | (head arr) > currMax  = getmax (drop 1 arr) (head arr)
-    | otherwise             = getmax (drop 1 arr) currMax
-
 
