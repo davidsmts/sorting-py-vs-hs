7a318656b8fae24bb2a859bd5895873146226dfb
David Schmotz <davidschmotz@gmx.de>
2020-04-11 15:57:18 +0200

    - conquer works

--- mergesort.hs
+++ mergesort.hs
@@ -1,12 +1,18 @@
-mergesort :: [Int] -> [Int]
+{- mergesort :: [Int] -> [Int]
 mergesort inp
     | length inp > 2    = mergesort (fst splitted) ++ mergesort (snd splitted)
     | otherwise         = conquer inp
     where splitted = splitAt ((length inp) 'div' 2) inp
-
+ -}
 
 conquer :: [Int] -> [Int] -> [Int]
+conquer [] [] = []
+conquer arr [] = arr
+conquer [] arr = arr
 conquer (x:xs) (y:ys)
-    | null (x:xs) = 
-    | x > y     = y : conquer ys (x:xs)
-    | x <= y    = x : conquer xs (y:ys)
+    | y < x     = y : (conquer (x:xs) ys)
+    | otherwise = x : (conquer xs (y:ys))
+
+
+test :: [Int] -> [Int]
+test (x:xs) = (x:xs)
